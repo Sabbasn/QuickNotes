d0d60a5e7def3a8ad1f85c038817f69a814f8740
Sabbasn <ben_7aus@hotmail.com>
2026-02-01 14:41:39 +0100

    Adjust position of ScrollFrame in Notepad UI

--- Source/Components/Notepad/UI/ScrollFrame.lua
+++ Source/Components/Notepad/UI/ScrollFrame.lua
@@ -1,6 +1,6 @@
 function Notepad:_InitializeScrollFrame()
 	self.Frame.ScrollFrame = CreateFrame("ScrollFrame", nil, self.Frame, "UIPanelScrollFrameTemplate")
-	self.Frame.ScrollFrame:SetPoint("TOPLEFT", self.Frame, "TOPLEFT", 0, -60)
+	self.Frame.ScrollFrame:SetPoint("TOPLEFT", self.Frame, "TOPLEFT", 10, -60)
 	self.Frame.ScrollFrame:SetPoint("BOTTOMRIGHT", self.Frame, "BOTTOMRIGHT", 0, 15)
 	self.Frame.ScrollFrame:SetClipsChildren(true)
 
